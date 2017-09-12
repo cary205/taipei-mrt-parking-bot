b56601294b7890f62b713a60a1c6d803d90eef4d
cary205 <cary205@hotmail.com>
2017-09-12 08:47:18 +0000

    add console log

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const app = express();
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
 app.post('/webhook', line.middleware(config), (req, res) => {
+  console.log("##### someone talk at " + Date());
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result));
